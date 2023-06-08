464e83f1b20fde69677f9c60da822dd8ada3d1ef
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-06-08 18:54:05 -0300

    sim

--- strings/1024-Criptografia.py
+++ strings/1024-Criptografia.py
@@ -1,10 +1,19 @@
-fraseInput = input()
-fraseCriptografada = ''
+indexLoop = 0
+quantidadePalavras = int(input())
 
-for i in fraseInput:
-    novoCaractere = chr(ord(i) + 3)
-    fraseCriptografada += novoCaractere
+while indexLoop < quantidadePalavras:
+    fraseInput = input()
+    fraseCriptografada = ""
 
-fraseCriptografada = fraseCriptografada[::-1]
+    for i in fraseInput:
+        novoCaractere = chr(ord(i) + 3)
+        fraseCriptografada += novoCaractere
 
-print(fraseCriptografada)
+    fraseCriptografada = fraseCriptografada[::-1]
+
+    for i in fraseCriptografada:
+        if fraseCriptografada.index(i) >= len(fraseCriptografada) // 2:
+            novoCaractere = chr(ord(i) - 1)
+            fraseCriptografada[fraseCriptografada.index(i)] = novoCaractere
+    indexLoop += 1
+    print(fraseCriptografada)
